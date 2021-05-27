280c0829c415084fe646fb52981d41163d880538
BRUCE Lee <44639709+black3279@users.noreply.github.com>
2021-05-27 23:46:47 +0900

    Add files via upload

---
+++ 1_Max Consecutive Ones.cpp
@@ -0,0 +1,45 @@
+#pragma warning(disable:4996)
+
+#if 01
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+vector<int> numsData;
+vector<int>& nums = numsData;
+
+class Solution {
+public:
+	int findMaxConsecutiveOnes(vector<int>& nums) {
+		int max = 0;
+		int continuous = 0;
+		int prevNum = -1;
+
+		for (int i = 0; i < nums.size(); i++){
+			if (nums[i] == 1){
+				if (prevNum == 1){
+					continuous++;
+				}
+				else{
+					continuous = prevNum = 1;
+				}
+
+				if (continuous>max) max = continuous;
+			}
+			else {
+				prevNum = nums[i];
+				continuous = 0;
+			}
+		}
+
+		return max;
+	}
+};
+
+
+#endif
